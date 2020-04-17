a4bd84d108be53f2479f45cec5b8071972b598a7
Archie Maclean <archiepiper@hotmail.com>
2020-04-17 09:46:41 +0100

    Incrementally improve security

--- granny_plant/settings.py
+++ granny_plant/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^z)q*zif8xdiw-0c_&mj8^4tn&p8nnlii7x#k%siqmyxkd3&rv'
+SECRET_KEY = environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
